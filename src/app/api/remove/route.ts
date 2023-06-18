7a6dd6b7536f4e5409c89923ef14f0be0d5e4c4c
brunofhorn <b.fernandeshorn@gmail.com>
2023-06-18 01:21:59 -0300

    fix: remove non used vars

--- src/app/api/remove/route.ts
+++ src/app/api/remove/route.ts
@@ -1,16 +1,6 @@
-import { Prisma } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 import { prisma } from "@/lib/prisma";
-import { processFacebookInput } from "@/utils/processFacebookInput";
-import { processGithubInput } from "@/utils/processGithubInput";
-import { processInstagramInput } from "@/utils/processInstagramInput";
-import { processLinkedinInput } from "@/utils/processLinkedinInput";
-import { processTikTokInput } from "@/utils/processTikTokInput";
-import { processTwitchInput } from "@/utils/processTwitchInput";
-import { processTwitterInput } from "@/utils/processTwitterInput";
-import { processWebsiteInput } from "@/utils/processWebsiteInput";
-import { processYoutubeInput } from "@/utils/processYoutubeInput";
 
 export async function POST(request: Request) {
     const { id } = await request.json();
