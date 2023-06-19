386be436183131fda624e4d3de6d6e355f9dc82c
brunofhorn <b.fernandeshorn@gmail.com>
2023-06-19 12:05:02 -0300

    feat: testing with fetch

--- src/app/api/instagram/route.ts
+++ src/app/api/instagram/route.ts
@@ -5,9 +5,14 @@ export async function GET(request: Request) {
     const { user } = await request.json();
 
     try {
-        const response: any = await axios.get(`https://www.instagram.com/${user}/?__a=1`);
+        const response = await fetch(`https://www.instagram.com/${user}/?__a=1`);
 
         console.log(response);
+
+        if (response.ok) {
+            const jsonData = await response.json();
+            console.log(jsonData);
+        }
         return NextResponse.json({ usuario: user });
     } catch (error) {
         console.log("ERROR CATCH - INSTAGRAM: ", error);
